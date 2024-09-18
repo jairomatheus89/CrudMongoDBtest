e7cfdbae313bf9e8f57e42d3c5e22ef0a71b8bdc
Jairo <jairomatheus89@Outlook.com>
2024-09-17 21:06:41 -0300

    second COmmit

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+function signIn() {
+    var user = document.getElementById("usernameInput").value;
+    var password = document.getElementById("passwordInput").value;
+
+    window.alert("Login done with success \nOn username: " + user + "\nAnd password: " + password);
+}
+
+function signUp() {
+    var user = document.getElementById("usernameInput").value;
+    var password = document.getElementById("passwordInput").value;
+
+    window.alert("Register done with success \nOn username: " + user + "\nAnd password: " + password);
+}
